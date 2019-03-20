301cd592f1fd594f560bee7c89049ed001a9b790
Wagaana Alex <wagaanaa@gmail.com>
2019-03-19 22:04:20 -0700

    Dashboard update

--- app/src/main/java/com/oasis/oasis/AppDashboad.java
+++ app/src/main/java/com/oasis/oasis/AppDashboad.java
@@ -3,6 +3,7 @@ package com.oasis.oasis;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
@@ -19,7 +20,33 @@ public class AppDashboad extends AppCompatActivity {
         setContentView(R.layout.activity_dashboad);
 
         grid = (GridView)findViewById(R.id.simpleGrid);
-        grid.setAdapter(new ImageAdapter(this,image,text));
+        grid.setAdapter(new ImageAdapter(this,image,text){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+
+                int color = 0x00FFFFFF; // Transparent
+                if (position==0) {
+                    color = 0xFFFF00FF;
+                }
+
+                if (position==1) {
+                    color = 0xFF8000FF;
+                }
+
+                if (position==2) {
+                    color = 0xFF000000;
+                }
+
+                if (position==3) {
+                    color = R.color.colorblue;
+                }
+
+                view.setBackgroundColor(color);
+
+                return view;
+            }
+        });
 
         grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
